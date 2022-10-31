6e65abc81e7c110844af97cbf41e0134a3b33413
olegs <oleg.s@bizzabo.com>
2022-10-31 20:40:41 +0200

    Add complex numbers

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.apache.commons.numbers.complex.Complex;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Complex a = Complex.ofCartesian(10, 20);
+        Complex b = Complex.ofCartesian(30, 40);
+
+        Complex c = a.subtract(b);
+
+        System.out.println("c = " + c);
     }
 }
